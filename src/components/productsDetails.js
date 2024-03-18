52a9df18c384fc76a8f051db79555eeabf3476ab
Ali Othman <alioth840@gmail.com>
2024-03-18 12:53:05 +0100

    Deling with props cheldern

--- src/components/productsDetails.js
+++ src/components/productsDetails.js
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 let productCount = 0;
 function displayFormattedProductsCount() {
     return productCount > 0 ? productCount : "zero";
@@ -9,9 +11,9 @@ function ProductsDetails(props) {
     return (
         <div className="d-flex align-items-center justify-content-between mt-1">
             <h6 className="font-weight-bold my-2">{props.price}</h6>
-            <button className="btn btn-primary">-</button>
+            <Button>-</Button>
             <span>{displayFormattedProductsCount()}</span>
-            <button className="btn btn-primary">+</button>
+            <Button>+</Button>
             <span className={badgClass}>{props.isAvilable ? "Avilable" : "Unavilable"}</span>
         </div>
     )        
